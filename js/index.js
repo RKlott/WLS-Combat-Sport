a8428eb79e80431bc42e182610e49589dffd4c12
RKlott <proklott@gmail.com>
2026-01-20 15:36:42 +0100

    modif style menu burger

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
+// variables pour target les éléments html sur lesquels intéragir
 const burgerBtn = document.getElementById('burgerBtn');
 const navMenu = document.getElementById('navMenu');
 
 burgerBtn.addEventListener('click', () => {
-    // On bascule la classe 'active' sur le bouton et le menu
+    // ajoute la classe 'active' sur le bouton et le menu
     burgerBtn.classList.toggle('active');
     navMenu.classList.toggle('active');
 });
 
-// Optionnel : Fermer le menu si on clique sur un lien
+// ferme le menu si on clique sur un lien
 document.querySelectorAll('.menuItem').forEach(link => {
     link.addEventListener('click', () => {
         burgerBtn.classList.remove('active');
