a204fa8ab69a8fc79353ff516c6e95a5c76bc861
louisr <louisboutetcamille@gmail.com>
2026-01-21 14:08:26 +0100

    js 1.0

--- js/index.js
+++ js/index.js
@@ -15,3 +15,37 @@ document.querySelectorAll('.menuItem').forEach(link => {
         navMenu.classList.remove('active');
     });
 });
+
+// // ANIMATION RETOUR HAUT DE PAGE
+document.querySelector('.anchor').addEventListener('click', function(e) {
+  e.preventDefault();
+  
+  const targetId = this.getAttribute('href').substring(1);
+  const targetElement = document.getElementById(targetId);
+  
+  if (targetElement) {
+    const startPosition = window.pageYOffset;
+    const targetPosition = targetElement.offsetTop;
+    const distance = targetPosition - startPosition;
+    const duration = 2000; // 5 secondes
+    let start = null;
+    
+    function animation(currentTime) {
+      if (start === null) start = currentTime;
+      const timeElapsed = currentTime - start;
+      const run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+    
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+    
+    requestAnimationFrame(animation);
+  }
+});
+// // FIN ANIMATION RETOUR HAUT DE PAGE
